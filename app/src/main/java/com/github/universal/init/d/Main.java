433bdd15613de4d349bcc36b1851581fa9fc4b07
Waester <Waester@users.noreply.github.com>
2017-10-12 15:32:32 +0200

    Cleanup testing code

--- app/src/main/java/com/github/universal/init/d/Main.java
+++ app/src/main/java/com/github/universal/init/d/Main.java
@@ -23,7 +23,6 @@ public class Main extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        bootReceiver.run("whoami");
         if (!prefs.getBoolean("enabled", false)) {
             Toast.makeText(this, getResources().getText(R.string.app_name) + " enabled", Toast.LENGTH_SHORT).show();
             prefsEdit.putBoolean("enabled", true);
